c9e4e6e3202273df1d14110dc44edf52b91117bd
LoloziAnas <lolozianas1@gmail.com>
2022-03-01 20:11:16 +0000

    designing score fragment

---
+++ app/src/main/java/com/lolozianas/missingletters/ui/game/ScoreFragment.kt
@@ -0,0 +1,27 @@
+package com.lolozianas.missingletters.ui.game
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lolozianas.missingletters.R
+
+
+class ScoreFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_score, container, false)
+    }
+
+
+}
